e7bd77753e3f82a8c92d0dc38601518737231646
PaulDom <54893606+PaulDom@users.noreply.github.com>
2022-12-11 11:16:20 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int health = 10;
+
+    //Метод, понижающий здоровье игрока
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        print("Здоровье игрока: " + health);
+    }
+}
+
