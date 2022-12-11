dd484ef7ab342d4ee1a0b218c608f43e97b5c990
PaulDom <54893606+PaulDom@users.noreply.github.com>
2022-12-11 12:01:24 +0300

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private int health = 10;
+    //сцена перезагрузки
+    public string sceneload;
+    //Объект, отвечающий за проигрывание звука
+    public AudioSource audioSource;
+    //Звуковой файл, содержащий звук урона
+    public AudioClip damageSound;
+
 
     //Метод, понижающий здоровье игрока
     public void TakeDamage(int damage)
     {
         health -= damage;
-        print("Здоровье игрока: " + health);
+
+        if (health > 0)
+        {
+            print("Здоровье игрока: " + health);
+            audioSource.PlayOneShot(damageSound);
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneload);
+        }
     }
 }
 
